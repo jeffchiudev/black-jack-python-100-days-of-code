19cdd91a741bc4a9a296c3cc19ae5985a60d1ab4
Jeff Chiu <jwchiu82@gmail.com>
2021-06-24 22:16:32 -0700

    finish black jack project

--- main.py
+++ main.py
@@ -22,15 +22,6 @@ def deal_card():
   dealt_card = random.choice(cards)
   return dealt_card
 
-#Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
-user_cards = []
-dealer_cards = []
-game_over = False
-
-for _ in range(2):
-  user_cards.append(deal_card())
-  dealer_cards.append(deal_card())
-
 #Hint 6: Create a function called calculate_score() that takes a List of cards as input 
 #and returns the score. 
 #Look up the sum() function to help you do this.
@@ -48,29 +39,63 @@ def calc_score(cards):
 
   return sum(cards)
 
-
-#Hint 9: Call calculate_score(). If the computer or the user has a blackjack (0) or if the user's score is over 21, then the game ends.
-user_score = calc_score(user_cards)
-dealer_score = calc_score(dealer_cards)
-print(f"   Your cards: {user_cards}, current score: {user_score}")
-print(f"   Dealer's first card: {dealer_cards[0]}")
-
-if user_score == 0 or dealer_score == 0 or user_score > 21:
-  game_over = True
-else: 
-  #Hint 10: If the game has not ended, ask the user if they want to draw another card. If yes, then use the deal_card() function to add another card to the user_cards List. If no, then the game has ended.
-  user_should_deal = input("Type 'y' to get another card, type 'n' to pass: ").lower()
-  if user_should_deal == "y":
-    user_cards.append(deal_card())
-  else:
-    game_over == True
-
-
-
-#Hint 11: The score will need to be rechecked with every new card drawn and the checks in Hint 9 need to be repeated until the game ends.
-
-#Hint 12: Once the user is done, it's time to let the computer play. The computer should keep drawing cards as long as it has a score less than 17.
-
 #Hint 13: Create a function called compare() and pass in the user_score and computer_score. If the computer and user both have the same score, then it's a draw. If the computer has a blackjack (0), then the user loses. If the user has a blackjack (0), then the user wins. If the user_score is over 21, then the user loses. If the computer_score is over 21, then the computer loses. If none of the above, then the player with the highest score wins.
+def compare(user_score, dealer_score):
+  if user_score == dealer_score:
+    return "Draw"
+  elif dealer_score == 0:
+    return "Dealer wins"
+  elif user_score == 0:
+    return "You Win!"
+  elif user_score > 21:
+    return "Bust, you lose."
+  elif dealer_score > 21:
+    return "Dealer busts, you win!"
+  elif user_score > dealer_score: 
+    return "You win!"
+  else:
+    return "You lose"
+
+def play_game():
+
+  print(logo)
+  
+  #Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
+  user_cards = []
+  dealer_cards = []
+  game_over = False
+
+  for _ in range(2):
+    user_cards.append(deal_card())
+    dealer_cards.append(deal_card())
+
+  #Hint 11: The score will need to be rechecked with every new card drawn and the checks in Hint 9 need to be repeated until the game ends.
+  while not game_over: 
+    #Hint 9: Call calculate_score(). If the computer or the user has a blackjack (0) or if the user's score is over 21, then the game ends.
+    user_score = calc_score(user_cards)
+    dealer_score = calc_score(dealer_cards)
+    print(f"   Your cards: {user_cards}, current score: {user_score}")
+    print(f"   Dealer's first card: {dealer_cards[0]}")
+
+    if user_score == 0 or dealer_score == 0 or user_score > 21:
+      game_over = True
+    else: 
+      #Hint 10: If the game has not ended, ask the user if they want to draw another card. If yes, then use the deal_card() function to add another card to the user_cards List. If no, then the game has ended.
+      user_should_deal = input("Type 'y' to get another card, type 'n' to pass: ").lower()
+      if user_should_deal == "y":
+        user_cards.append(deal_card())
+      else:
+        game_over = True
+
+  #Hint 12: Once the user is done, it's time to let the computer play. The computer should keep drawing cards as long as it has a score less than 17.
+  while dealer_score != 0 and dealer_score < 17:
+    dealer_cards.append(deal_card())
+    dealer_score = calc_score(dealer_cards)
+
+  print(f"  Your final hand: {user_cards}, your score: {user_score}")
+  print(f"  Dealer final hand: {dealer_cards}, dealer score: {dealer_score}")
+  print(compare(user_score, dealer_score))
 
 #Hint 14: Ask the user if they want to restart the game. If they answer yes, clear the console and start a new game of blackjack and show the logo from art.py.
+while input("Would you like to play a game of Blackjack? Type 'y'  or 'n': ") == "y":
+  play_game()
